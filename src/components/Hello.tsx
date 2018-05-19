dee247a798496fa93781d6f15d2cfa42fb61978c
wengwengweng <593110501@qq.com>
2018-05-19 23:02:35 +0800

    feat: familiar with ts

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -2,29 +2,34 @@ import * as React from "react";
 
 export interface HelloProps { compiler: string; framework: string; }
 
+type ButtonSize = 'small' | 'default' | 'large';
+
+type State = { isOpen: boolean; num: number; }
+// export interface State { isOpen: boolean; num: number; }
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
-export class Hello extends React.Component<HelloProps, {
-    isOpen: boolean
-}> {
+export class Hello extends React.Component<HelloProps, State> {
     constructor(props: HelloProps) {
         super(props)
         this.state = {
-            isOpen: false
+            isOpen: false,
+            num: 0
         }
     }
 
     handleClick = () => {
         this.setState({
-            isOpen: !this.state.isOpen
+            isOpen: !this.state.isOpen,
+            num: this.state.num + 1
         })
     }
 
     render() {
-        const { isOpen } = this.state
+        const { isOpen, num } = this.state
         return (
             <div>
                 <p>{isOpen ? '已打开' : '已关闭'}</p>
+                <p>{`点击次数：${num}`}</p>
                 <button onClick={this.handleClick}>{isOpen ? '关闭' : '打开'}</button>
             </div>
         )
